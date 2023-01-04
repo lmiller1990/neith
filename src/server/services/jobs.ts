7e107504ef0385550169cac1ef12818259532c97
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-01-04 20:54:56 +1000

    wip - basic scheduler

--- src/server/services/jobs.ts
+++ src/server/services/jobs.ts
@@ -1,4 +1,7 @@
 import { Knex } from "knex";
+import { EventEmitter } from "node:events";
+import TypedEmitter from "typed-emitter";
+import { DateTime } from "luxon";
 import { schedule } from "../../../dbschema.js";
 
 //  * In addition, see if any jobs that should have been run were **not** run,
@@ -9,18 +12,95 @@ import { schedule } from "../../../dbschema.js";
  * Get a list of all jobs and the next scheduled date.
  */
 
-interface Job {
+export interface Job {
   name: string;
-  startsAt: string;
-  lastRunAt: string;
+  organizationId: number;
   schedule: schedule;
+  timezone: string;
+  callback : () => Promise<any>
+  doneCallback?: () => void
 }
 
-interface DeriveJobsPayload {
-  jobs: Job[];
+export function millisUntilNextMonday(
+  serverTimeNow: DateTime,
+  timezone: string
+) {
+  const monday = 1;
+  let d = serverTimeNow.setZone(timezone);
+  while (d.weekday !== monday) {
+    d = d.plus({ day: 1 }).set({
+      hour: 9,
+      minute: 0,
+      second: 0,
+      millisecond: 0,
+    });
+  }
+
+  return d.diff(serverTimeNow, "milliseconds").toMillis();
 }
 
-export async function deriveJobs(db: Knex, payload: DeriveJobsPayload) {
-  const jobs = await db("jobs");
-  // ...
+interface JobToRun {
+  /**
+   * Number of milliseconds until this job should execute.
+   */
+  runInMillis: number;
+  /**
+   * Organization that job belongs to.
+   */
+  organizationId: number;
+
+  callback: () => Promise<any>
+
+  doneCallback?: () => void
+}
+
+export function deriveJobs(
+  now: DateTime,
+  jobs: Job[]
+): JobToRun[] {
+  return jobs.map((job) => {
+    const runInMillis = millisUntilNextMonday(now, job.timezone);
+    return {
+      ...job,
+      runInMillis,
+    };
+  });
+}
+
+// type EmitterEvents = {
+//   "job:add": () => void;
+// };
+
+// / extends (EventEmitter as new () => TypedEmitter<EmitterEvents>) {
+class Scheduler {
+  #jobs = new Map<number, NodeJS.Timeout>();
+
+  schedule(
+    id: number,
+    runInMillis: number,
+    job: () => Promise<void>,
+    doneCallback?: () => void
+  ) {
+    const timeoutID = global.setTimeout(async () => {
+      await job();
+      doneCallback?.();
+    }, runInMillis);
+
+    this.#jobs.set(id, timeoutID);
+  }
+}
+
+const scheduler = new Scheduler();
+
+export function scheduleJobs(jobs: JobToRun[]) {
+  for (const job of jobs) {
+    scheduler.schedule(
+      job.organizationId,
+      job.runInMillis,
+      job.callback,
+      job.doneCallback
+    );
+
+    return scheduler
+  }
 }
--- test/server/services/jobs.spec.ts
+++ test/server/services/jobs.spec.ts
@@ -1,7 +1,144 @@
-import { describe, it } from "vitest";
+import { DateTime } from "luxon";
+import { describe, expect, it } from "vitest";
+import {
+  deriveJobs,
+  Job,
+  millisUntilNextMonday,
+  scheduleJobs,
+} from "../../../src/server/services/jobs.js";
 
 describe("deriveJobs", () => {
-  it("gets jobs with the next scheduled execution time", () => {
-    //
+  it("handles weekly job", () => {
+    let i = 0;
+    const inc = () => Promise.resolve(i++);
+    const jobs: Job[] = [
+      {
+        name: "job_one",
+        organizationId: 1,
+        timezone: "utc",
+        schedule: "weekly",
+        callback: inc,
+      },
+    ];
+
+    // 7th of Jan 2023 is a Saturday.
+    const now = DateTime.fromObject(
+      {
+        year: 2023,
+        month: 1,
+        day: 7,
+        hour: 9,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      },
+      { zone: "utc" }
+    );
+
+    const actual = deriveJobs(now, jobs);
+
+    expect(actual).toMatchInlineSnapshot(`
+      [
+        {
+          "callback": [Function],
+          "name": "job_one",
+          "organizationId": 1,
+          "runInMillis": 172800000,
+          "schedule": "weekly",
+          "timezone": "utc",
+        },
+      ]
+    `);
   });
+});
+
+describe("millisUntilNextMonday", () => {
+  it("works when now is in previous week", () => {
+    // 8th of Jan 2023 is a Sunday.
+    const now = DateTime.fromObject(
+      {
+        year: 2023,
+        month: 1,
+        day: 8,
+        hour: 9,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      },
+      { zone: "utc" }
+    );
+
+    const actual = millisUntilNextMonday(now, "utc");
+
+    // 24 hours
+    expect(actual).toBe(86400000);
+  });
+
+  it("works when now is in previous week", () => {
+    // 8th of Jan 2023 is a Sunday.
+    const now = DateTime.fromObject(
+      {
+        year: 2023,
+        month: 1,
+        day: 8,
+        hour: 9,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      },
+      { zone: "America/New_York" }
+    );
+
+    const actual = millisUntilNextMonday(now, "America/New_York");
+
+    // 24 hours
+    expect(actual).toBe(86400000);
+  });
+
+  it("gets correct value when comparing utc and specific timezone", () => {
+    // 8th of Jan 2023 is a Sunday.
+    const now = DateTime.fromObject(
+      {
+        year: 2023,
+        month: 1,
+        day: 8,
+        hour: 9,
+        minute: 0,
+        second: 0,
+        millisecond: 0,
+      },
+      { zone: "utc" }
+    );
+
+    const actual = millisUntilNextMonday(now, "America/New_York");
+
+    // 29h => 104400000ms
+    expect(actual).toBe(104400000);
+  });
+});
+
+describe("scheduleJobs", () => {
+  it("schedules and runs job", () =>
+    new Promise<void>((done) => {
+      let i = 0;
+      const inc = () => Promise.resolve(i++);
+
+      let complete = false;
+      const doneCallback = () => (complete = true);
+
+      const scheduler = scheduleJobs([
+        {
+          runInMillis: 1000,
+          organizationId: 1,
+          callback: inc,
+          doneCallback,
+        },
+      ]);
+
+      global.setTimeout(() => {
+        expect(complete).toBe(true)
+        expect(i).toBe(1)
+        done()
+      }, 1500)
+    }));
 });
