ae3de2ce7313f3b5faa00866a7eadc392de482f4
Lachlan Miller <lachlan.miller.1990@outlook.com>
2023-01-09 23:19:22 +1000

    fix: use correct email

--- src/server/services/mailer.ts
+++ src/server/services/mailer.ts
@@ -7,7 +7,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: 465,
   secure: true,
-  from: "no-reply@neith.dev",
+  from: "hello@neith.dev",
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
@@ -20,7 +20,7 @@ export const Mailer = {
     debug("Sending email at %s", d.toISOString());
     return transporter.sendMail({
       to: "hello@neith.dev",
-      from: "no-reply@neith.dev",
+      from: "hello@neith.dev",
       subject: "Testing the dep notifier app!",
       text: `Hi! This email was sent at #toISOString: ${d.toISOString()}. #toLocaleString: ${d.toLocaleString()}`,
     });
